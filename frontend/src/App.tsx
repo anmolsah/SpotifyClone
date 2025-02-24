5d7a2b6dce618db7abba349ad99aa5b1f6091caa
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-25 02:24:52 +0530

    chore: update TypeScript ESLint packages to version 8.25.0 and modify App component styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 
-
 function App() {
-  
+
 
   return (
     <>
-      <h1 className="text-5xl text-center text-green-600">Hello world </h1>
+     <h1 className="text-3xl font-bold underline text-red-600 text-center">Hello Ji</h1>
     </>
   )
 }
