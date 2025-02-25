961a0cd771d9af638b5dddfd3ad0e090b7b53f5b
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-26 00:51:25 +0530

    feat: implement utility functions and button component with Tailwind CSS styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import { Button } from "@/components/ui/button";
 
 function App() {
-
-
   return (
-    <>
-     <h1 className="text-3xl font-bold underline text-red-600 text-center">Hello Ji</h1>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold underline text-red-600 text-center">
+        Hello Ji
+      </h1>
+      <Button className="mt-4">Click Me</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
