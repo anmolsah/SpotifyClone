7cb9ffbbe00fd8b28fc1989406b6c80432e471a2
Anmol Sah <kumarxxx242424@gmail.com>
2025-02-26 01:01:14 +0530

    feat: integrate Clerk authentication and update App component for user sign-in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold underline text-red-600 text-center">
-        Hello Ji
-      </h1>
-      <Button className="mt-4">Click Me</Button>
-    </div>
+    <>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button></SignInButton> 
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
   );
 }
 
